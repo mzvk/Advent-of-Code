6c90bcf9472e7957ab0806e8944f495b9c45f344
mzvk <m.zukey@gmail.com>
2018-12-14 11:05:55 +0100

    d2

---
+++ 2015/aoc02.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+data_in = 'inputs/aoc02.in'
+
+def load(file):
+  with open(file) as x: output = x.read()
+  return [ol for ol in output.split('\n') if ol]
+
+def solve(data):
+  (tp, rb) = (0, 0)
+  for pkg in data:
+    pkg = sorted([int(val) for val in pkg.split('x')])
+    tp += 2*pkg[0]*pkg[1] + 2*pkg[0]*pkg[2] + 2*pkg[1]*pkg[2] + pkg[0]*pkg[1]
+    rb += 2*pkg[0] + 2*pkg[1] + pkg[0]*pkg[1]*pkg[2]
+  print "Pt1: {}\nPt2: {}".format(tp, rb)
+
+solve(load(data_in))
