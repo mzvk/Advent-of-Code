95aefcc53d30566bffe2da453d23e7843814df92
Maciej <m.zukey@gmail.com>
2018-12-17 21:22:53 +0100

    day1

--- 2015/aoc01.py
+++ 2015/aoc01.py
@@ -9,12 +9,10 @@ def load(file):
 
 def solve(data):
   (ff, fb) = (0, 0)
-
   for idx, move in enumerate(data):
     try: ff = act[move](ff)
     except KeyError: pass
     if ff < 0 and not fb: fb = idx + 1
-
   print "Pt1: {}\nPt2: {}".format(ff, fb)
 
 solve(load(data_in))
