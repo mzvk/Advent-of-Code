d7ffdd6841ceb8f20190ff3a15d9702e54a748e7
Maciej <m.zukey@gmail.com>
2018-12-24 13:45:09 +0100

    day5

--- 2015/aoc05.py
+++ 2015/aoc05.py
@@ -12,7 +12,7 @@ def solve(data):
   output = [0, 0]
   for word in data:
     if re.search(r'([a-z])\1', word) and not re.search(r'ab|cd|pq|xy', word) and re.search(r'[aeiou].*[aeiou].*[aeiou]', word): output[0] += 1
-    if re.search(r'([a-z][a-z]).*\1', word) and re.search(r'([a-z]).\1', word): output[1] += 1
+    if re.search(r'([a-z]{2}).*\1', word) and re.search(r'([a-z]).\1', word): output[1] += 1
   print "Pt1: {}\nPt2: {}".format(output[0], output[1])
 
 solve(load(data_in))
