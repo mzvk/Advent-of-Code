882eb7e09252bf8f41dcc990ae4e81680b9ed37f
Maciej <m.zukey@gmail.com>
2018-12-25 22:24:23 +0100

    day6

--- 2015/aoc06.py
+++ 2015/aoc06.py
@@ -6,6 +6,12 @@ def load(file):
   with open(file) as x: output = x.read()
   return [ol for ol in output.split('\n') if ol]
 
+def gridInit():
+   grid = []
+   for x in xrange(1000):
+      grid.append([0] * 1000)
+   return grid
+
 def solve(data):
   pass
 
