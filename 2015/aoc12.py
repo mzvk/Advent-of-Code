f41d0c279d0362e51e211844684ced5e6ed74d42
mzvk <m.zukey@gmail.com>
2018-12-26 22:45:02 +0100

    day12

---
+++ 2015/aoc12.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+data_in = 'inputs/aoc12.in'
+
+def load(file):
+  with open(file) as x: output = x.read()
+  return output.replace('\n', '')
+
+def solve(data):
+  total = 0
+  seq = ''
+  for char in data:
+    if char == '-': seq = '-'
+    else:
+      try:
+        int(char)
+        seq += char
+      except ValueError:
+        if seq:
+          total += int(seq)
+          seq = ''
+  print "Pt1: {}".format(total)
+
+solve(load(data_in))
