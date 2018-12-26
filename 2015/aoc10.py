4e0779f6a7aef78f7d7b7bd17c44121299abaebb
mzvk <m.zukey@gmail.com>
2018-12-26 22:11:59 +0100

    brutes

--- 2015/aoc10.py
+++ 2015/aoc10.py
@@ -4,26 +4,27 @@ data_in = 'inputs/aoc10.in'
 
 def load(file):
   with open(file) as x: output = x.read()
-return output.replace('\n', '')
+  return output.replace('\n', '')
 
 def look_say(start):
   (current, counter) = ('', 0)
   new = ''
-  
+
   for digit in start:
     if current != digit:
       if current: new = '{}{}{}'.format(new, counter, current)
       current = digit
       counter = 1
     else: counter += 1
-    
+
   return '{}{}{}'.format(new, counter, current)
-  
+
 def solve(data):
   start = data
   for x in xrange(50):
+    print "now: {}".format(x)
     start = look_say(start)
-    if x == 39: part1 = len(start) 
-  print "Pt1:{}, len(start)
-  
+    if x == 39: part1 = len(start)
+  print "Pt1:{}\nPt2:{}".format(part1, len(start))
+
 solve(load(data_in))
