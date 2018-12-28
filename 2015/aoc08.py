0db90ed5e69f578eddaec949cdadf4fc1081ff91
mzvk <m.zukey@gmail.com>
2018-12-28 12:09:51 +0100

    day8

--- 2015/aoc08.py
+++ 2015/aoc08.py
@@ -7,14 +7,11 @@ def load(file):
   return [ol for ol in output.split('\n') if ol]
 
 def solve(data):
-  pass
+  (tmem, tstr, tnst) = (0, 0, 0)
+  for line in data:
+    tstr += len(line)
+    tmem += len(line.decode('string_escape')) - 2
+    tnst += len(repr(line.replace(r'"', r'."')))
+  print "Pt1: {}\nPt2: {}".format(tstr - tmem, tnst - tstr)
 
-data = load(data_in)
-
-(tmem, tstr, tnst) = (0, 0, 0)
-for line in data:
-  tstr += len(line)
-  tmem += len(line.decode('string_escape')) - 2
-  tnst += len(repr(line.replace(r'"', r'."')))
-
-print "Pt1: {}\nPt2: {}".format(tstr - tmem, tnst - tstr)
+solve(load(data_in))
