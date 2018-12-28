9fa3397cfa680267d53013c26a24f4e1c653ba73
mzvk <m.zukey@gmail.com>
2018-12-28 12:02:01 +0100

    day8

--- 2015/aoc08.py
+++ 2015/aoc08.py
@@ -6,6 +6,9 @@ def load(file):
   with open(file) as x: output = x.read()
   return [ol for ol in output.split('\n') if ol]
 
+def solve(data):
+  pass
+
 data = load(data_in)
 
 (tmem, tstr, tnst) = (0, 0, 0)
@@ -13,10 +16,5 @@ for line in data:
   tstr += len(line)
   tmem += len(line.decode('string_escape')) - 2
   tnst += len(repr(line.replace(r'"', r'."')))
-#  tmem += len(repr(line.replace(r'\"', r'\."'))) + 2
 
-print "Total chars of str: {} [23]".format(tstr)
-print "Total chars in mem: {} [11]".format(tmem)
-print "Total new repr:     {} [xx]".format(tnst)
-print "String literals overhead 1: {}".format(tstr - tmem)
-print "String literals overhead 2: {}".format(tnst - tstr)
+print "Pt1: {}\nPt2: {}".format(tstr - tmem, tnst - tstr)
