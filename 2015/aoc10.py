ca40b9c421d8d74a4c037bdde083b7fda628acfd
Maciej <m.zukey@gmail.com>
2018-12-30 17:35:03 +0100

    day10

--- 2015/aoc10.py
+++ 2015/aoc10.py
@@ -7,27 +7,13 @@ def load(file):
   with open(file) as x: output = x.read()
   return output.replace('\n', '')
 
-def seqsplit(seq):
-  pass
-
-def look_say(start):
-  (current, counter) = ('', 0)
-  new = ''
-
-  for digit in start:
-    if current != digit:
-      if current: new = '{}{}{}'.format(new, counter, current)
-      current = digit
-      counter = 1
-    else: counter += 1
-
-  return '{}{}{}'.format(new, counter, current)
+def lnslen(els):
+  dl = 0
+  for el in els:
+    dl += len(elems[el][0])
+  return dl
 
 def solve(data):
-  start = data
-  for x in xrange(50):
-    start = look_say(start)
-    if x == 39: part1 = len(start)
-  print "Pt1:{}\nPt2:{}".format(part1, len(start))
+  pass
 
 solve(load(data_in))
