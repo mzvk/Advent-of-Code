b35a0bf37457abfe595c43bb081f4a25c2dd28dc
mzvk <m.zukey@gmail.com>
2019-01-15 08:24:15 +0100

    day4

--- 2015/aoc04.py
+++ 2015/aoc04.py
@@ -1,30 +1,40 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
-import hashlib, sys, os, thread
+import hashlib, threading, sys
 
 data_in = sys.argv[1] if len(sys.argv[1:]) > 0 else 'inputs/set01/aoc04.in'
-gensfx = lambda s = 0: (str(s + 1*x) for x, _ in enumerate(iter(int, 1)))
+tcount = 128
+output = [0, 0]
+gensfx = lambda s = 0, y = 1: (s + y*x for x, _ in enumerate(iter(int, 1)))
+
+class dohash(threading.Thread):
+  def __init__(self, offset, data):
+    threading.Thread.__init__(self)
+    self.offset = offset
+    self.data = data
+  def run(self):
+    for sufix in gensfx(self.offset, tcount):
+      hash = hashlib.md5("{}{}".format(self.data, sufix)).hexdigest()
+      if hash[:5] == '0' * 5:
+        if not output[0]: output[0] = sufix
+        elif sufix < output[0]: output[0] = sufix
+      if hash[:6] == '0' * 6:
+        if not output[1]: output[1] = sufix
+        elif sufix < output[1]: output[1] = sufix
+      if (output[0] and sufix > output[0]) and (output[1] and sufix > output[1]): break
 
 def load(file):
   with open(file) as x: output = x.read()
   return output.replace('\n', '')
 
 def solve(data):
-  progress = '\|/-\|/-'
-  output = [0, 0]
-  for sufix in gensfx():
-    if(hashlib.md5("{}{}".format(data, sufix)).hexdigest()[:5] == '0' * 5 and not output[0]): output[0] = sufix
-    if(hashlib.md5("{}{}".format(data, sufix)).hexdigest()[:6] == '0' * 6 and not output[1]): output[1] = sufix
-    if(output[0] and output[1]):
-      sys.stdout.write("\rPt1: {n1:{width}}\nPt2: {n2:{width}}".format(n1=output[0], n2=output[1], width=len(sufix)))
-      sys.stdout.flush()
-      return 0
-    sys.stdout.write("\r [{}] {}".format(progress[int(sufix) % 8], sufix))
-    sys.stdout.flush()
+  gLock = threading.Lock()
+  try:
+    threads = [dohash(tid, 'yzbqklnj') for tid in xrange(tcount)]
+    [t.start() for t in threads]
+  except:
+    print "Unable to start threads"
+  [t.join() for t in threads]
+  print "Pt1: {}\nPt2: {}".format(output[0], output[1])
 
-t_count = 2
-try:
-  os.system('setterm -cursor off')
-  solve(load(data_in))
-finally:
-  os.system('setterm -cursor on')
+solve(load(data_in))
---
+++ 2015/brutes/b_aoc04.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import hashlib, sys, os, thread
+
+data_in = sys.argv[1] if len(sys.argv[1:]) > 0 else 'inputs/set01/aoc04.in'
+gensfx = lambda s = 0: (str(s + 1*x) for x, _ in enumerate(iter(int, 1)))
+
+def load(file):
+  with open(file) as x: output = x.read()
+  return output.replace('\n', '')
+
+def solve(data):
+  progress = '\|/-\|/-'
+  output = [0, 0]
+  for sufix in gensfx():
+    if(hashlib.md5("{}{}".format(data, sufix)).hexdigest()[:5] == '0' * 5 and not output[0]): output[0] = sufix
+    if(hashlib.md5("{}{}".format(data, sufix)).hexdigest()[:6] == '0' * 6 and not output[1]): output[1] = sufix
+    if(output[0] and output[1]):
+      sys.stdout.write("\rPt1: {n1:{width}}\nPt2: {n2:{width}}".format(n1=output[0], n2=output[1], width=len(sufix)))
+      sys.stdout.flush()
+      return 0
+    sys.stdout.write("\r [{}] {}".format(progress[int(sufix) % 8], sufix))
+    sys.stdout.flush()
+
+t_count = 2
+try:
+  os.system('setterm -cursor off')
+  solve(load(data_in))
+finally:
+  os.system('setterm -cursor on')
