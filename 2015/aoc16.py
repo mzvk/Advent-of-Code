f80ff123d64aa72febd9a31a91e066faed7f10a6
mzvk <m.zukey@gmail.com>
2019-06-17 11:01:33 +0200

    aoc16

--- 2015/aoc16.py
+++ 2015/aoc16.py
@@ -3,7 +3,28 @@
 import re
 
 data_in = "inputs/aoc16.in"
-mfcsam = {'children': 3, 'cats': 7, 'samoyeds': 2, 'pomeranians': 3, 'akitas': 0, 'vizslas': 0, 'goldfish': 5, 'trees': 3, 'cars': 2, 'perfumes': 1}
+
+
+mfcsam = {'children': (3, '=', '='),
+          'cats': (7, '=', '<'),
+          'samoyeds': (2, '=', '='),
+          'pomeranians': (3, '=', '>'),
+          'akitas': (0, '=', '='),
+          'vizslas': (0, '=', '='),
+          'goldfish': (5, '=', '>'),
+          'trees': (3, '=', '<'),
+          'cars': (2, '=', '='),
+          'perfumes': (1, '=', '=')}
+
+
+mfcsamV = {'children': 3, 'cats': 7, 'samoyeds': 2, 'pomeranians': 3, 'akitas': 0, 'vizslas': 0, 'goldfish': 5, 'trees': 3, 'cars': 2, 'perfumes': 1}
+mfcsamT = (('=', '='),('=', '>'),('=', '='),('=', '<'),('=', '='),('=', '='),('=', '<'),('=', '>'),('=', '='),('=', '='))
+funarr = {'<': lambda x,y: x < y,
+          '>': lambda x,y: x > y,
+          '=': lambda x,y: x == y}
+
+#cats, trees >
+#pomeranians, goldfish <
 
 class regex(object):
   def __or__(self, other):
@@ -22,12 +43,35 @@ def load(file):
   return output
 
 def solve(input):
+  result = [1, 1]
+  lock = [0, 0]
   for sueid, sue in enumerate(input):
-    notsue = 1
+    clear = [0, 0]
     for detect in mfcsam:
       if detect in sue:
-        if not mfcsam[detect] == sue[detect]: notsue = 0
-    if notsue: break
-  print sueid + 1
+
+        if not lock[0]:
+          if funarr[mfcsam[detect][1]](mfcsam[detect][0],sue[detect]): result[0] = sueid + 1
+          else: clear[0] = 1
+
+        if not lock[1]:
+          if funarr[mfcsam[detect][2]](mfcsam[detect][0],sue[detect]): result[1] = sueid + 1
+          else: clear[1] = 1
+
+        if clear[0] and clear[1]:
+          result = [0, 0]
+          break
+
+    else:
+        if clear[0]: result[0] = 0
+        else: lock[0] = 1
+
+        if clear[1]: result[1] = 0
+        else: lock[1] = 1
+
+    if result[0] * result[1]: break
+
+  print result[0]
+  print result[1]
 
 solve(load(data_in))
