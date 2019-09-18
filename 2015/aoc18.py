3ad92bf6937e4e66c5e1d0e4b0c7e113c6185413
mzvk <m.zukey@gmail.com>
2019-09-18 14:09:59 +0200

    aoc18

---
+++ 2015/aoc18.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import sys, copy
+
+data_in = sys.argv[1] if len(sys.argv[1:]) > 0 else 'inputs/set01/aoc18.in'
+
+MAX_EPOCH = 100
+_GRIDX = 100
+_GRIDY = 100
+newborns = {}
+
+def load(file):
+  with open(file) as x: output = x.read()
+  flock = []
+  for idx, pos in enumerate(output.replace('\n','')):
+    if pos == '#': flock.append(idx)
+  return flock
+
+def solve(data, epoch_max, epoch=0):
+  global newborns
+  flock = {k:[1,0] for k in data}
+  while(epoch < epoch_max):
+    new_flock = {}
+    newborns = {}
+    for cell in flock:
+      if cell >= _GRIDX * _GRIDY: continue
+      flock[cell][1] = countN(cell, flock)
+      if flock[cell][1] >= 2 and flock[cell][1] <= 3: new_flock[cell] = [1,0]
+    for cell in newborns:
+      if newborns[cell] == 3: new_flock[cell] = [1,0]
+    flock = copy.deepcopy(new_flock)
+    epoch += 1
+  print "Pt1: {}\nPt2: {}".format(len(flock), 0)
+
+def countN(cell, flock):
+   neighbors = set([-_GRIDX+1,-_GRIDX,-_GRIDX-1,+1,_GRIDX+1,_GRIDX,_GRIDX-1,-1])
+   result = 0
+   if(cell / _GRIDY == 0):          neighbors -= {-_GRIDX-1, -_GRIDX, -_GRIDX+1} ## upper
+   if(cell / _GRIDY == _GRIDY - 1): neighbors -= { _GRIDX-1,  _GRIDX,  _GRIDX+1} ## lower
+   if(cell % _GRIDX == 0):          neighbors -= {-_GRIDX-1,      -1,  _GRIDX-1} ## leftmost
+   if(cell % _GRIDX == _GRIDX - 1): neighbors -= {-_GRIDX+1,       1,  _GRIDX+1} ## righmost
+
+   for neighbor in neighbors:
+     if neighbor + cell in flock: result += 1
+     else: newborns[neighbor + cell] = newborns[neighbor + cell] + 1 if neighbor + cell in newborns else 1
+   return result
+
+solve(load(data_in), MAX_EPOCH)
