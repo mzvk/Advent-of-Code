69ab148f063781e993ec6e0137756c0ca65bdd01
mzvk <m.zukey@gmail.com>
2020-12-06 00:20:27 +0100

    aoc23

--- 2015/aoc23.py
+++ 2015/aoc23.py
@@ -2,41 +2,29 @@
 
 import sys, time
 
-#registers:
-a = 0
-b = 0
-
-iset = {
-  'hlf': lambda x: x/2,
-  'tpl': lambda x: x*3,
-  'inc': lambda x: x+1,
-  'jmp': 0,
-  'jie': 0,
-  'jio': 0,
-}
-
-#    jmp offset is a jump; it continues with the instruction offset away relative to itself.
-#    jie r, offset is like jmp, but only jumps if register r is even ("jump if even").
-#    jio r, offset is like jmp, but only jumps if register r is 1 ("jump if one", not odd).
-
 data_in = sys.argv[1] if len(sys.argv[1:]) > 0 else 'inputs/set01/aoc23.in'
-
-def parser(ins, *args):
-   print "{} -> {}".format(ins, args)
 
 def load(file):
   with open(file) as x: output = x.read()
   return [line for line in output.split('\n') if line]
 
-def solve(data):
+def solve(data, regs):
   ip = 0
-  result = [0, 0]
   while(ip < len(data)):
-#    print "{:02} instruction: {}".format(ip, data[ip])
-    if data[ip][:3] == 'jmp': ip = max(0, ip + int(data[ip][4:]))
-    else: ip += 1
-    print ip
-    time.sleep(1)
-  print "Pt1: {}\nPt2: {}".format(result[0], result[1])
+    tokens = [x.replace(',','') for x in data[ip].split()]
+    if   (tokens[0] == 'hlf'):
+      regs[tokens[1]]/=2
+      ip+=1
+    elif (tokens[0] == 'tpl'):
+      regs[tokens[1]]*=3
+      ip+=1
+    elif (tokens[0] == 'inc'):
+      regs[tokens[1]]+=1
+      ip+=1
+    elif (tokens[0] == 'jmp'): ip+=int(tokens[1].replace('+',''))
+    elif (tokens[0] == 'jio'): ip+=int(tokens[2].replace('+','')) if regs[tokens[1]] == 1 else 1
+    elif (tokens[0] == 'jie'): ip+=1 if regs[tokens[1]] % 2  else int(tokens[2].replace('+',''))
+  return regs
 
-solve(load(data_in))
+data=load(data_in)
+print "Pt1: {}\nPt2: {}".format(solve(data, {'a': 0, 'b': 0})['b'],solve(data, {'a': 1, 'b': 0})['b'])
